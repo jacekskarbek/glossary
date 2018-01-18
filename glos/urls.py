eb1324df98eae2b3d4476a14828157b0b6d24c96
jacekskarbek <jaceks@lspsoftware.pl>
2018-01-18 13:34:15 +0100

    18.01.2018a

--- glos/urls.py
+++ glos/urls.py
@@ -16,5 +16,15 @@ urlpatterns = [
     url(r'^api-token-auth/', authviews.obtain_auth_token),
     url(r'^create_post/', views.create_post),
     url(r'^edit/(\d+)/$', views.editentry, name='editentry'),
+    url(r'^terms/', views.term_list),
+    url(r'^user_termbase/', views.user_termbase_list),
+    url(r'^language/', views.language_list),
+    url(r'^termbase/', views.termbase_list),
+    url(r'^create_termbase/', views.termbase_create),
+    url(r'^dtype_update/', views.dtype_update),
+    url(r'^delete_termbase/', views.termbase),
+    url(r'^create_term/', views.term_create),
+    url(r'^create_term_merge/', views.term_create_merge),
+    url(r'^create_language/', views.language_create),
 ]
 
