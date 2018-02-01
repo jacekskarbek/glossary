9151e207e1393db3ce9a0442030168a89d59fc3a
jacekskarbek <jaceks@lspsoftware.pl>
2018-02-01 13:14:22 +0100

    2018-02-01

--- glossary/settings.py
+++ glossary/settings.py
@@ -47,6 +47,17 @@ INSTALLED_APPS = [
  
 ]
 
+EMAIL_BACKEND = config('EMAIL_BACKEND', default='django.core.mail.backends.smtp.EmailBackend')
+EMAIL_HOST = config('EMAIL_HOST', default='')
+EMAIL_PORT = config('EMAIL_PORT', default=587, cast=int)
+EMAIL_HOST_USER = config('EMAIL_HOST_USER', default='')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD', default='')
+EMAIL_USE_TLS = config('EMAIL_USE_TLS', default=True, cast=bool)
+
+DEFAULT_FROM_EMAIL = 'LocStar <kontakt-test@locstar.pl >'
+#SERVER_EMAIL = 'info@locstar.pl'
+EMAIL_SUBJECT_PREFIX = '[LocStar] '
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
--- locstar/forms.py
+++ locstar/forms.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*- 
 from django import forms
-
+from django.utils.translation import ugettext_lazy as _
 
 class ContactForm(forms.Form):
     #searchTerm = forms.CharField(label='Term: ', label_suffix="", max_length=100, widget=forms.TextInput(attrs={'class' : 'form-control input-cls'}))
-    name = forms.CharField(required=True, label='', label_suffix="", max_length=100, widget=forms.TextInput(attrs={'class' : 'contactSearchBar', 'placeholder': 'Your Name'}))
-    email = forms.EmailField(required=True, label='', label_suffix="", max_length=100, widget=forms.EmailInput(attrs={'class' : 'contactSearchBar', 'placeholder': 'Email Address'}))
-    message = forms.CharField(required=True, label='', label_suffix="", max_length=100, widget=forms.Textarea(attrs={'class' : 'contactSearchBar', 'placeholder': 'Your message'}))
+    name = forms.CharField(required=True, label='', label_suffix="", max_length=100, widget=forms.TextInput(attrs={'class' : 'contactSearchBar', 'placeholder': _('Imię i nazwisko')}), localize=True    )
+    email = forms.EmailField(required=True, label='', label_suffix="", max_length=100, widget=forms.EmailInput(attrs={'class' : 'contactSearchBar', 'placeholder': _('Adres email')}), localize=True    )
+    message = forms.CharField(required=True, label='', label_suffix="", max_length=100, widget=forms.Textarea(attrs={'class' : 'contactSearchBar', 'placeholder': _('Wiadomość')}), localize=True    )
